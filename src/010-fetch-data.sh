60e09ea4e993f92f103862ab8a7037b145e1eb31
Tony Lee <iamtonylee@gmail.com>
2025-09-22 23:43:35 -0700

    Added some sanity check

--- src/010-fetch-data.sh
+++ src/010-fetch-data.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "interest-rates" ]; then
-    curl -L https://www.interactivebrokers.ca/en/accounts/fees/pricing-interest-rates.php
+    curl -sL https://www.interactivebrokers.ca/en/accounts/fees/pricing-interest-rates.php
 elif [ "$1" == "margin-rates" ]; then
-    curl -L https://www.interactivebrokers.ca/en/trading/margin-rates.php
+    curl -sL https://www.interactivebrokers.ca/en/trading/margin-rates.php
 else
     echo "Invalid argument"
     exit 1
